34c11f6f8c741bd04d98b09d94ed7afe6f76ae14
Sabin <127272916+Sabin404@users.noreply.github.com>
2024-02-27 22:46:09 +0545

    Add files via upload

---
+++ qrCode/script.js
@@ -0,0 +1,19 @@
+const inputValue = document.querySelector('#inputValue');
+const img = document.querySelector('.image');
+const btn = document.querySelector('.btn');
+let p = document.querySelector('p');
+
+function generateQr() {
+  if (inputValue.value.length > 0) {
+    const value = inputValue.value.trim();
+    let URL = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${value}`;
+    img.src = URL;
+    img.classList.add('image1');
+    p.innerHTML=''
+  }else{
+    p.innerHTML="Enter Value ";
+    img.classList.add('image')
+  }
+}
+
+btn.addEventListener('click', generateQr)
